03eb5bd83fe1e742bba20eb30d1dab080ad57375
RWR <rwr8106@gmail.com>
2022-09-16 16:46:41 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Lesson 2, Part 4 - Skillcrush
+
+// In this challenge, you’ll test your new arithmetic operator and template literal skills. Your friend in Canada just told you the temperature at their home is 10 degrees Celsius. However, you’re accustomed to measuring temperatures in Fahrenheit. Agh! So what should you do? Build a Celsius to Fahrenheit converter with JavaScript, of course!
+
+var tempCelsius = 10;
+var toFahrenheit = tempCelsius * 1.8 + 32;
+
+console.log(
+  `Outside my friend’s house it’s ${tempCelsius}°C, which is ${toFahrenheit}°F`
+);
